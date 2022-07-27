778a0f8e84e846409c8329cdd0ed9804045e693f
fadi Badawi <ifadbad@gmail.com>
2022-07-27 12:16:06 +0400

    ---

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,23 +1,38 @@
-import type { Handle } from '@sveltejs/kit';
-import * as cookie from 'cookie';
+import type { GetSession, Handle, HandleError } from "@sveltejs/kit";
+import Logger from "$lib/bw/api/logger"
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-	event.locals.userid = cookies['userid'] || crypto.randomUUID();
 
-	const response = await resolve(event);
-
-	if (!cookies['userid']) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		response.headers.set(
-			'set-cookie',
-			cookie.serialize('userid', event.locals.userid, {
-				path: '/',
-				httpOnly: true
-			})
-		);
+	const { url, locals, clientAddress, params, platform, request } = event
+	const query = event.url.searchParams
+	
+	const headers = event.request.headers
+	const bearer = headers.get('authorization')
+	
+	if(bearer){
+		event.locals.token = bearer.replace('Bearer ', '');
 	}
 
+	const response = await resolve(event);
 	return response;
 };
+
+export const getSession: GetSession = (event) => {
+    return {
+      	token: event.locals.token ?? ''
+    }
+}
+
+export const handleError:HandleError = async ({ error, event }) => {
+	// Sentry.captureException(error, { event });
+	Logger.log({
+		level: 'error', 
+		status: 500, 
+		message: error.message, 
+		timestamp: new Date( Date.now() ).toISOString(),
+		name: error.name,
+		frame: error.frame,
+		...event,
+		stack: error.stack,
+	})
+}
