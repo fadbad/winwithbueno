e000778963bd57e3a13b6232715affb8f2679645
fadi Badawi <ifadbad@gmail.com>
2022-07-29 13:40:44 +0400

    ---

---
+++ src/routes/privacy.svelte
@@ -0,0 +1,166 @@
+<script>
+    import { Template } from "$lib"
+</script>
+
+<Template title="Privacy Policy">
+    <div class="container mx-4 md:mx-auto max-w-5xl pb-20">
+        <h1>Privacy Policy</h1>
+        Effective Date: August 10th 2020
+
+        <p>
+            It is our aim through this PEPSICO AFRICA MIDDLE EAST SOUTH ASIA PRIVACY POLICY (&quot;Policy&quot;) to explain what information we collect about you and why we collect that information. We also want to make sure you understand how we use and share your
+            information and the choices available to you. Our goal is to earn and keep your trust as you visit websites and applications about our products and brands.
+        </p>
+        <p>
+            This Policy describes how we treat personal information on websites (&quot;sites&quot;), mobile or social media applications (&quot;apps&quot;), and any other services where we include an authorized link or reference to this Policy (together, the &quot;Services&quot;).
+            This Policy does not describe our practices on sites or apps where it is not referenced.
+        </p>
+        <p>
+            Your use of our apps and sites indicates you agree to our collection, use, and sharing of your information as set forth in this Policy.
+        </p>
+        <h2> What information do we collect?
+        </h2>
+        <p>
+            <strong>We collect identifiers and contact information.</strong> For example, we may collect your name and street address if you register on our site or apps. We may also collect your phone number or email address. We also collect online identifiers.
+            For example, if you engage with us through a social media site, we may collect your social media user identifier.
+        </p>
+        <p>
+
+            <strong>We collect information you submit or otherwise provide or authorize us to collect.</strong> We collect the information you post in a public space on our site. This includes comments and videos or photos. We also collect any other information
+            you may provide to us or authorize us to collect. This may include information about your physical activities or your nutrition preferences.
+        </p>
+        <p>
+            <strong>We collect demographic information and information about your interests.</strong> We may collect information such as your gender and age, household or lifestyle information, and your ZIP or postal code.
+        </p>
+        <p>
+            <strong>We collect internet and electronic network activity information, such as device and usage information.</strong> We may collect information about the browser you are using, and we may look at what site you came from, or what site you visit
+            when you leave us. We may also collect information about your operating system, Internet Service Provider (ISP) information and IP addresses and other device identifiers, such as mobile device identifiers. In addition, we may collect information
+            about your use of our Services, through use of tracking technologies such as cookies, pixel tags, and Flash Cookies.
+        </p>
+        <p>
+            <strong> 
+            We collect geolocation information.</strong> Using technologies such as GPS or Wi-Fi, we may collect information about your location. For example, if you use our mobile apps, we may access your device&#39;s location information to provide
+            you location-enabled features.
+
+        </p>
+        <h2>
+            How do we collect information?</h2>
+
+        We collect information directly from you. We collect information from you when you:
+        <ul>
+
+            <li>make a purchase with us;</li>
+            <li>register on one of our sites or apps;</li>
+            <li> join our loyalty programs;</li>
+            <li> sign up for our email newsletters and other notifications;</li>
+            <li> participate in promotions such as contests, according to the terms and conditions of such promotion or contest;</li>
+            <li> fill out a survey;</li>
+            <li> contact us, such as through our sites and apps or otherwise communicating with our Consumer Relations team; and</li>
+            <li>otherwise provide it to us or authorize us to collect it</li>
+        </ul>
+        <p>
+            <strong> collect information from you through tracking tools.</strong> We use tracking tools like browser cookies and mobile device IDs to collect information when you use the Services. We do this on our websites, in emails that we send to you,
+            and in media advertising. We collect information about usage and browser information over time.
+        </p>
+        <p>
+            <strong> We combine information from different sources. </strong>For example, we may combine information we have collected offline with information we collect online. We combine information we have collected across third-party sites, including
+            social media sites. We combine information across devices such as computers and mobile devices. We may also combine information from third parties with information we already have. We may use this information to generate inferences about you and
+            for the other purposes described in this Privacy Policy.
+        </p>
+        <h2>How Do We Use the Information We Collect?</h2>
+
+        <p>
+            <strong> We use information to help us effectively provide you products and services.</strong> We use your information to respond to your questions or enable you to participate in our programs. This may include promotions and contests. We also
+            may use your information to process your registration with our sites and apps. We may use your friends&#39; email addresses to send them information that you requested we send them provided they consent to the same. We also use your information
+            to process orders for you. We may also contact you about products and services you have requested, or if you have won a prize or promotion.
+        </p>
+        <p>
+            <strong> We use information to improve our products and services. </strong>One of our goals is that you have a positive experience with our sites, apps, and products. Therefore, we may use your information to improve your experience with our sites,
+            apps, and products. We may use information you provide or that third-party partners give us to personalize your experience provided that such third parties obtained your consent to do so.
+        </p>
+        <p>
+            <strong> We use information for marketing purposes. </strong>For example, we might send you information about products, offers, features, or updates. We might also use your information to serve you ads about products and offers. Those ads may
+            appear on third-party platforms which may include social media. These might be our own offers or products. We may provide you with information through email or push notification.
+        </p>
+        <p>
+            <strong> We use information to get feedback from you about how we&#39;re doing and for other transactional purposes such as registration completion and similar actions.</strong> We may contact you about this Policy or the Terms of Use posted to
+            our sites or apps that you are using. We may also communicate with you about a job application or your questions about our products or promotions.
+        </p>
+        <p>
+            <strong> We use information as the law allows us and as stated in this Policy.</strong>
+        </p>
+        <h2>
+            How Do We Share Your Information?</h2>
+        <p>
+            <strong> We share information within the PepsiCo family of companies and brands all over the world.</strong> This includes current and future PepsiCo Inc. subsidiaries, affiliates, and joint venture partners. This also includes current brands
+            Beverage and snacks, and all future brands. For a list of PepsiCo brands, visit https://www.pepsico.com/brands/product-information. We may also share information with third parties with which we have an ownership interest or commercial relationship,
+        </p>
+        <p>
+            <strong> We share information with our business partners and third-party vendors.</strong> For example, we may share information with joint promotion partners. If you register for a joint promotion, your information may be collected by both us
+            and the third party. This might include retailers. Your information will be used by us as described in this Policy or by promotion partners as described in their policies. We may also share information with companies who serve as our vendors or
+            third parties who perform services on our behalf. They may help us deliver products or services to you. We may share information with companies who operate our sites and apps or who run our promotions.
+        </p>
+        <p>
+            <strong> We will share information if we think we must in order to comply with the law or to protect our company. </strong>For example, we will share information to respond to a court order or subpoena. We may share information if a government
+            agency or investigatory body requests it. This includes United States and non-United States law enforcement or regulatory authorities. We may also share information to help protect you, our sites and apps, our company, our customers, and our consumers.
+            We may also share information if we are investigating potential fraud.
+        </p>
+        <p>
+            <strong> Third parties may collect information when you use the Services. </strong>Third parties that provide advertising, content, and/or functionality to our Services may collect information from these Services through tracking tools like browser
+            cookies and mobile device IDs. For example, third-party advertisers may have a presence on some of our Services, and this information may be collected over time and combined with information collected across different websites and online services.
+        </p>
+        <p>
+            <strong> We may share information with any successor to all or part of our business. </strong>For example, if part of our business is sold, or is involved in a merger, consolidation, asset sale, or in the unlikely event of bankruptcy, we may include
+            your information as part of that transaction.
+        </p>
+        <h2>
+            You Have Certain Choices About How We Use Your Information
+        </h2>
+        <p>
+            <strong> You can opt out of email marketing. </strong>To stop receiving promotional emails from us, follow the instructions in any promotional email message you get from us. If you opt out of getting marketing email messages, you may continue
+            to receive non-marketing email messages from us where permitted by law and you may opt out of getting non-marketing email messages as well.
+        </p>
+        <p>
+            <strong> You can control tools on your mobile devices.</strong> For example, you can turn off the GPS locator or push notifications on your phone. To modify the push notifications you get from our app, you can alter your preferences in your profile.
+        </p>
+        <h2>Children&#39;s Privacy.</h2>
+        <p>
+            It is not our intention to collect personal information from children under age 12. If you are a parent or legal guardian and think that your child under 12 has given us information, you can contact us at amesasupport@pepsico.com
+        </p>
+        <h2>
+            Security Measures
+        </h2>
+        <p>
+            We use various technical and organizational mechanisms to secure our websites and apps. You should understand that no data storage system or transmission of data over the Internet or any other public network can be guaranteed to be perfectly secure.
+        </p>
+        <h2>
+            We May Link to Other Sites or Apps or Have Third Party Services and Tools on Our Platform THAT We Don't Control
+        </h2>
+        <p>We may link to or allow you to access the Services from third-party websites or platforms. Our sites and apps may also include third-party content that collects information. Third party content may include tools like cookies and tracking technologies.
+            These third parties may use your information for their own purposes. This may include behavioral advertising and analytic purposes. We do not control these third parties or their use of these technologies, so please read their privacy policies
+            carefully.
+        </p>
+        <h2>We May Update This Policy</h2>
+        <p>
+            From time to time, we may change this Policy. When we do, we will notify you of any material changes as required by law. This will include posting an updated copy on our websites, applications and platforms . Please check periodically for updates.
+        </p>
+    </div>
+</Template>
+<style>
+    h1, h2 {
+        font-weight: bold;
+    }
+    h1 {
+        font-size: 36px;
+    }
+    h2 {
+        font-size: 24px;
+    }
+    p {
+        margin-bottom: 10px;
+    }
+    ul {
+        padding: 0 20px 10px;
+        list-style: circle;
+    }
+</style>
